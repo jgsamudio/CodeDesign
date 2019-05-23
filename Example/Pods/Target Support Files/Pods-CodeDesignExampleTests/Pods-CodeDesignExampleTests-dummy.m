53b3be875a602c10d8bb9fae029149ee86c675d0
jgsamudio <jonathan2457@gmail.com>
2019-05-22 21:46:34 -0400

    Reload example project.

---
+++ Example/Pods/Target Support Files/Pods-CodeDesignExampleTests/Pods-CodeDesignExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodeDesignExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_CodeDesignExampleTests
+@end
--- Example/Pods/Target Support Files/Pods-CodeDesign_Tests/Pods-CodeDesign_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodeDesign_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CodeDesign_Tests
-@end
